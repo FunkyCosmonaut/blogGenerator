ad2d3cbe1774908069ec1b2c15eb8e7291d73b31
Christopher Munoz <cmunoz9@jamiroquai.gi.alaska.edu>
2023-03-04 05:10:04 -0900

    functions and type signatures

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn (makeHtml "dicks" "pussy")
+
+myhtml :: String
+myhtml = html_ (body_ "Hello, world!")
+
+wrapHtml :: String -> String
+wrapHtml content = "<html><body>" <>content<> "</body></html>"
+
+html_ :: String -> String
+html_ content = "<html>" <>content<> "</html>"
+
+body_ :: String -> String
+body_ content = "<body>" <>content<> "</body>"
+
+head_ :: String -> String
+head_ content ="<head>"<> content<> "</head>"
+
+title_ :: String -> String
+title_ content ="<title>" <>content<> "</title>"
+
+makeHtml :: String -> String -> String
+makeHtml title body = html_ ((head_ (title_ title)) <> (body_ body))
